a2f5020d5cbf87ea8377017676c94abb7b8f9526
Kontrast16 <salavdi.pi@mail.ru>
2026-04-18 23:31:26 +0300

    1

--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-    "fmt"
-    "sync"
-)
-
 func main() {
-    var wg sync.WaitGroup
-    counter := 0  // общая переменная
-    
-    for i := 0; i < 1000; i++ {
-        wg.Add(1)
-        go func() {
-            counter++  // <- РЕАЛЬНАЯ ОНКА! несколько горутин пишут одновременн
-            wg.Done()
-        }()
-    }
-    
-    wg.Wait()
-    fmt.Println("Counter:", counter)
+
 }
 
 func MaxInt(a, b int) int {
     if a >= b {
         return a
     }
+
     return b
 }
