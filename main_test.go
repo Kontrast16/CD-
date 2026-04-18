5adf797c3a93d2f7b30e235e6fb3d8643a885f62
Kontrast16 <salavdi.pi@mail.ru>
2026-04-18 23:08:57 +0300

    Update

--- main_test.go
+++ main_test.go
@@ -6,17 +6,17 @@ import (
 )
 
 // ЭТОТ ТЕСТ ПОКАЖЕТ ГОНКУ!
-func TestWithRealRace(t *testing.T) {
+
+// А ЭТОТ ТЕСТ НЕ ПОКАЖЕТ ГОНКУ (как ваш код)
+func TestWithoutRace(t *testing.T) {
     var wg sync.WaitGroup
-    counter := 0
     
-    for i := 0; i < 100; i++ {
+    for i := 0; i < 5; i++ {
         wg.Add(1)
         go func() {
-            counter++ // ← ПРОБЛЕМА: конкурентная запись
+            _ = i // только чтение, нет записи
             wg.Done()
         }()
     }
-    
     wg.Wait()
 }
