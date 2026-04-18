7ebd6312c36303f40b4fd98fe7dd31e7ae2b3cf1
Kontrast16 <salavdi.pi@mail.ru>
2026-04-18 23:08:41 +0300

    Update

--- main_test.go
+++ main_test.go
@@ -20,17 +20,3 @@ func TestWithRealRace(t *testing.T) {
     
     wg.Wait()
 }
-
-// А ЭТОТ ТЕСТ НЕ ПОКАЖЕТ ГОНКУ (как ваш код)
-func TestWithoutRace(t *testing.T) {
-    var wg sync.WaitGroup
-    
-    for i := 0; i < 5; i++ {
-        wg.Add(1)
-        go func() {
-            _ = i // только чтение, нет записи
-            wg.Done()
-        }()
-    }
-    wg.Wait()
-}
